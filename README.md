222d213f86501c8afd25a8d4f25bef349a71e6e5
Max Jorink <maxjorink@gmail.com>
2026-01-18 23:34:19 +0100

    Inital Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# VitalShift
+A mod that changes your avatar based on your health, allows for visual damage to player if paired with the right avatars.
+Last tested on Melonloader 0.6.5, BoneLab patch 6.
+
+## Current Features
+- Avatar Selection based on health.
+- Disable dying (you still get damage, the last blow just won't kill you)
+- Disible automatic healing
+
+## Recommended mods
+Health pickups: ...
+Avatars with damage states, this is mine: ...
+
+## Discord Server
+Join the Discord server to chat, give suggestions, report issues, and get sneak peeks!
+https://discord.gg/Tu7Pnq4RV7
---
+++ VitalShift/Setup.cs
@@ -0,0 +1,35 @@
+﻿using MelonLoader;
+using BoneLib.BoneMenu;
+using BoneLib.Notifications;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(VitalShift.Core), "VitalShift", "1.0.0", "jorink")]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+
+namespace VitalShift {
+    public partial class Core : MelonMod {
+
+        public override void OnInitializeMelon() {
+            SetupMelonPreferences();
+            SetupBoneMenu();
+        }
+
+        private void SetupBoneMenu() {
+            Page defaultPage = Page.Root.CreatePage("VitalShift", Color.yellow);
+
+            Page defaultPage = defaultPage.CreatePage("Settings (Custom)", Color.cyan);
+            defaultPage.CreateFunction("Save Settings", Color.cyan, () => { MelonPreferences.Save(); });                   
+        }
+
+        private void SetupMelonPreferences() {
+            category = MelonPreferences.CreateCategory("VitalShift");
+            MelonPreferences.Save();
+            category.SaveToFile();
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+
+        }
+    }
+}
