6d7b68b19577bafbe9bb4ce9bf9491400a1f16ac
Max Jorink <maxjorink@gmail.com>
2026-01-19 01:16:09 +0100

    Health logging and first immortality test

--- VitalShift/Setup.cs
+++ VitalShift/Setup.cs
@@ -1,7 +1,7 @@
 ﻿using MelonLoader;
-using BoneLib.BoneMenu;
-using BoneLib.Notifications;
+using BoneLib;
 using UnityEngine;
+using Il2CppSLZ;
 
 [assembly: MelonInfo(typeof(VitalShift.Core), "VitalShift", "1.0.0", "jorink")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
@@ -15,14 +15,15 @@ namespace VitalShift {
         }
 
         private void SetupBoneMenu() {
-            Page defaultPage = Page.Root.CreatePage("VitalShift", Color.yellow);
+            BoneLib.BoneMenu.Page defaultPage = BoneLib.BoneMenu.Page.Root.CreatePage("VitalShift", Color.yellow);
 
-            Page defaultPage = defaultPage.CreatePage("Settings (Custom)", Color.cyan);
+            defaultPage.CreateBool("Immortal", Color.green, ImmortalEntry.Value, (a) => { ImmortalEntry.Value = a; });            
             defaultPage.CreateFunction("Save Settings", Color.cyan, () => { MelonPreferences.Save(); });                   
         }
 
         private void SetupMelonPreferences() {
             category = MelonPreferences.CreateCategory("VitalShift");
+            ImmortalEntry = category.CreateEntry("Immortal", false);
             MelonPreferences.Save();
             category.SaveToFile();
         }
@@ -31,5 +32,21 @@ namespace VitalShift {
             base.OnSceneWasLoaded(buildIndex, sceneName);
 
         }
+
+        public override void OnUpdate() {
+            Immortal();
+        }
+    
+        private void Immortal() {
+            if (!ImmortalEntry.Value) return;
+            if (Time.time < lastcheck + 1f) return;
+            lastcheck = Time.time;
+
+            MelonLogger.Msg("Health: " + Player.RigManager.health.curr_Health);
+
+            if (Player.RigManager.health.curr_Health <= 10f) {
+                Player.RigManager.health.curr_Health = 20f;
+            }
+        }  
     }
 }
---
+++ VitalShift/Variables.cs
@@ -0,0 +1,14 @@
+using MelonLoader;
+using UnityEngine;
+
+namespace VitalShift {
+    public partial class Core : MelonMod {
+
+        MelonPreferences_Category category;
+
+        MelonPreferences_Entry<bool> ImmortalEntry;
+
+        private float lastcheck = 0f;
+
+    }
+}
