9c15cd29e4fa1618ad56ee16cf1d4cc52b2e52a0
Jorink <maxjorink@gmail.com>
2026-09-01 22:57:17 +0200

    Fix AssemblyInfo colors

--- Mod/Properties/AssemblyInfo.cs
+++ Mod/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using vitalshift;
 
 [assembly: MelonInfo(typeof(VitalShift), "VitalShift", VitalShift.Version, "Jorink")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
-[assembly: MelonColor(255,0,0,1)]
-[assembly: MelonAuthorColor(255,0,0,1)]
+[assembly: MelonColor(255,255,0,0)]
+[assembly: MelonAuthorColor(255,255,0,0)]
